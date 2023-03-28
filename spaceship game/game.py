ac772fc26b17cd9df06c7042817816b0ed39f6e3
ben-ka <62483741+ben-ka@users.noreply.github.com>
2023-03-28 11:03:30 +0300

    Update game.py

--- spaceship game/game.py
+++ spaceship game/game.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import time
 
 pygame.init()
 pygame.font.init()
@@ -18,10 +19,13 @@ WINNER_FONT = pygame.font.SysFont('comicsans', 200)
 FPS = 60
 VEL = 9
 BULLET_VELOCITY = 40
-MAX_BULLETS = 3
-
+MAX_BULLETS = 2
+YELLOW_INMUNITY_COOLDOWN = 10000
+RED_INMUNITY_COOLDOWN = 10000
 YELLOW_HIT = pygame.USEREVENT+1
 RED_HIT = pygame.USEREVENT+2
+YELLOW_BLOCKED = pygame.USEREVENT+3
+RED_BLOCKED = pygame.USEREVENT+4
 space =pygame.transform.scale(pygame.image.load(os.path.join("Assets","space.png")), (WIDTH, HEIGHT)) 
 
 SPACESHIP_WIDTH,SPACESHIP_HEIGHT=160,90
@@ -31,11 +35,15 @@ yellow_spaceship =  pygame.transform.rotate(yellow_spaceship,90)
 red_spaceshipImg = pygame.image.load(os.path.join("Assets","spaceship_red.png"))
 red_Spaceship = pygame.transform.scale(red_spaceshipImg, (SPACESHIP_WIDTH,SPACESHIP_HEIGHT))
 red_Spaceship = pygame.transform.rotate(red_Spaceship,270)
+yellow_shieldImg = pygame.image.load(os.path.join("Assets","yellow_shield.png"))
+yellow_shield = pygame.transform.scale(yellow_shieldImg, (30,20))
+red_shieldImg = pygame.image.load(os.path.join("Assets","red_shield.png"))
+red_shield = pygame.transform.scale(red_shieldImg, (30,20))
 
 BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join("Assets","bullet.mp3"))
 BULLET_SHOOT_SOUND = pygame.mixer.Sound(os.path.join("Assets","bullet2.mp3"))
 
-def draw_window(yellow,red, yellow_bullets,red_bullets,yellow_health,red_health,winner_text):
+def draw_window(yellow,red, yellow_bullets,red_bullets,yellow_health,red_health,winner_text,is_yellow_inmune,is_red_inmune):
     WIND.blit(space,(0,0))
     red_health_text = HEALTH_FONT.render(str(red_health), True, WHITE)
     yellow_health_text = HEALTH_FONT.render(str(yellow_health), True, WHITE)
@@ -46,6 +54,10 @@ def draw_window(yellow,red, yellow_bullets,red_bullets,yellow_health,red_health,
     WIND.blit(yellow_spaceship,(yellow.x,yellow.y))
     WIND.blit(red_Spaceship,(red.x,red.y))
     pygame.draw.rect(WIND,(0,0,0),BORDER) # black border
+    if (is_yellow_inmune):
+        WIND.blit(yellow_shield,(yellow.x+SPACESHIP_WIDTH//2,yellow.y))
+    if (is_red_inmune):
+        WIND.blit(red_shield,(red.x - SPACESHIP_WIDTH//2,red.y))
     
     for bullet in yellow_bullets:
           pygame.draw.rect(WIND,(255,255,0),bullet) # yellow bullet
@@ -53,6 +65,29 @@ def draw_window(yellow,red, yellow_bullets,red_bullets,yellow_health,red_health,
           pygame.draw.rect(WIND,(255,0,0),bullet) # red bullet    
      
     pygame.display.update()
+
+def handle_offscreen(yellow,red):
+    if yellow.x < 0:
+            yellow.x = 0
+    if yellow.x > WIDTH-SPACESHIP_WIDTH:
+            yellow.x = WIDTH-SPACESHIP_WIDTH
+    if red.x < 0:
+            red.x = 0
+    if red.x > WIDTH-SPACESHIP_WIDTH+50:
+            red.x = WIDTH-SPACESHIP_WIDTH+50
+    if yellow.y < 0:
+             yellow.y = 0
+    if yellow.y > HEIGHT-SPACESHIP_HEIGHT -40:
+            yellow.y = HEIGHT-SPACESHIP_HEIGHT -40
+    if red.y < 0:
+            red.y = 0
+    if red.y > HEIGHT-SPACESHIP_HEIGHT -40:
+            red.y = HEIGHT-SPACESHIP_HEIGHT -40
+    if yellow.x > WIDTH/2-70:
+          yellow.x = WIDTH/2-70
+    if red.x < WIDTH/2+5:
+          red.x = WIDTH/2+5
+
 
 def move_yellow(yellow,keys_pressed):
      if keys_pressed[pygame.K_a]: #left
@@ -100,40 +135,31 @@ def handle_bullets (yellow_bullets,red_bullets,yellow_spaceship,red_Spaceship):
                    
 
 
-
-def handle_offscreen(yellow,red):
-    if yellow.x < 0:
-            yellow.x = 0
-    if yellow.x > WIDTH-SPACESHIP_WIDTH:
-            yellow.x = WIDTH-SPACESHIP_WIDTH
-    if red.x < 0:
-            red.x = 0
-    if red.x > WIDTH-SPACESHIP_WIDTH+50:
-            red.x = WIDTH-SPACESHIP_WIDTH+50
-    if yellow.y < 0:
-             yellow.y = 0
-    if yellow.y > HEIGHT-SPACESHIP_HEIGHT -40:
-            yellow.y = HEIGHT-SPACESHIP_HEIGHT -40
-    if red.y < 0:
-            red.y = 0
-    if red.y > HEIGHT-SPACESHIP_HEIGHT -40:
-            red.y = HEIGHT-SPACESHIP_HEIGHT -40
-    if yellow.x > WIDTH/2-70:
-          yellow.x = WIDTH/2-70
-    if red.x < WIDTH/2+5:
-          red.x = WIDTH/2+5
+        
+           
+           
 
 
 def main():
+    pygame.init()
+    pygame.mixer.init()
+    
+    
     yellow = pygame.Rect(400,100,SPACESHIP_WIDTH,SPACESHIP_HEIGHT)
     red = pygame.Rect(1600,500,SPACESHIP_WIDTH,SPACESHIP_HEIGHT)
     red_bullets =[]
     yellow_bullets =[]
     red_health =10
     yellow_health =10
-    clock=pygame.time.Clock()
+    is_yellow_inmune = False
+    is_red_inmune = False
+    imnune_red_time =0
+    imnune_yellow_time = 0
     run = True
     winner_text=""
+    last_yellow_inmune = 0
+    last_red_inmune = 0
+    clock =pygame.time.Clock()
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -151,21 +177,46 @@ def main():
                         if winner_text =="":
                                 red_bullets.append(red_bullet)
                                 BULLET_SHOOT_SOUND.play()
-            if event.type == YELLOW_HIT:
-                 yellow_health-=1   # red hit yellow
-                 BULLET_HIT_SOUND.play()
-            if event.type == RED_HIT:
-                red_health-=1   # yellow hit red
+                  if event.type == pygame.K_e:
+                         current_time_yellow = pygame.time.get_ticks()
+                         if current_time_yellow > YELLOW_INMUNITY_COOLDOWN +last_yellow_inmune :
+                                pygame.event.post(pygame.event.Event(YELLOW_BLOCKED))
+                                imnune_yellow_time = current_time_yellow + 2000                                
+                                last_yellow_inmune = current_time_yellow                        
+                                is_yellow_inmune = True
+                  if event.type == pygame.K_RSHIFT:
+                         current_time_red = pygame.time.get_ticks()
+                         if current_time_red > RED_INMUNITY_COOLDOWN +last_red_inmune :
+                                pygame.event.post(pygame.event.Event(RED_BLOCKED))
+                                imnune_red_time = current_time_red + 2000                                
+                                last_red_inmune = current_time_red                       
+                                is_red_inmune = True
+
+
+
+
+
+
+            if event.type == YELLOW_HIT :   #        and imnune_yellow_time > 0
+                yellow_health-=1      # red hit yellow
+                BULLET_HIT_SOUND.play()
+            if event.type == RED_HIT :   #and imnune_red_time > 0
+                red_health-=1          # yellow hit red
                 BULLET_HIT_SOUND.play()    
-       
+            if is_yellow_inmune and imnune_yellow_time < pygame.time.get_ticks():
+                is_yellow_inmune = False
+            if is_red_inmune and imnune_red_time < pygame.time.get_ticks():
+                is_red_inmune = False
         if red_health<=0:
+             red_health-=1
              winner_text = "YELLOW WINS!"
         if yellow_health<=0:
+              yellow_health-=1  
               winner_text = "RED WINS!" 
         if winner_text!= "":    
                 print_winner(winner_text)
-                
                 break
+
          
       
 
@@ -175,8 +226,9 @@ def main():
         move_red(red,keys_pressed)
         handle_offscreen(yellow,red)
         handle_bullets(yellow_bullets,red_bullets,yellow,red)
-        draw_window(yellow,red,yellow_bullets,red_bullets ,yellow_health,red_health,winner_text)
+        draw_window(yellow,red,yellow_bullets,red_bullets ,yellow_health,red_health,winner_text , is_yellow_inmune,is_red_inmune)
     main()  
 
 if __name__ == '__main__':
     main()
+
